38365891f81e4aa0058198ff4f6a953d7ebc12eb
Givanio <GivanioMelo@users.noreply.github.com>
2024-09-25 16:37:48 -0300

    Just verifying...
    ... if i really commited the last updates!

--- TextUtils.py
+++ TextUtils.py
@@ -1,26 +1,37 @@
 import os
 
-BLACK = '\033[30m'
-RED = '\033[31m'
-GREEN = '\033[32m'
-YELLOW = '\033[33m' # orange on some systems
-BLUE = '\033[34m'
-MAGENTA = '\033[35m'
-CYAN = '\033[36m'
-LIGHT_GRAY = '\033[37m'
-DARK_GRAY = '\033[90m'
-BRIGHT_RED = '\033[91m'
-BRIGHT_GREEN = '\033[92m'
-BRIGHT_YELLOW = '\033[93m'
-BRIGHT_BLUE = '\033[94m'
-BRIGHT_MAGENTA = '\033[95m'
-BRIGHT_CYAN = '\033[96m'
-WHITE = '\033[97m'
+#Colors
+RESET           = '\033[0m'
 
-RESET = '\033[0m' # called to return to standard terminal text color
+#Foreground colors
+BLACK           = '\033[30m'
+RED             = '\033[31m'
+GREEN           = '\033[32m'
+YELLOW          = '\033[33m'
+BLUE            = '\033[34m'
+MAGENTA         = '\033[35m'
+CYAN            = '\033[36m'
+LIGHT_GRAY      = '\033[37m'
+DARK_GRAY       = '\033[90m'
+BRIGHT_RED      = '\033[91m'
+BRIGHT_GREEN    = '\033[92m'
+BRIGHT_YELLOW   = '\033[93m'
+BRIGHT_BLUE     = '\033[94m'
+BRIGHT_MAGENTA  = '\033[95m'
+BRIGHT_CYAN     = '\033[96m'
+WHITE           = '\033[97m'
+
+#Background colors
+BG_BLACK    = '\033[40m'
+BG_RED      = '\033[41m'
+BG_GREEN    = '\033[42m'
+BG_YELLOW   = '\033[43m'
+BG_BLUE     = '\033[44m'
+BG_MAGENTA  = '\033[45m'
+BG_CYAN     = '\033[46m'
+BG_GRAY     = '\033[47m'
 
 def printColored(s:str, color:str = WHITE, end="\n"): print(color + s + RESET, end=end)
-
 def printBlack(s:str, end="\n"):			printColored(s,BLACK, end=end)
 def printRed(s:str, end="\n"):				printColored(s,RED, end=end)
 def printGreen(s:str, end="\n"):			printColored(s,GREEN, end=end)
